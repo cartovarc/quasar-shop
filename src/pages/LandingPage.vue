fac1f3a22f64ec548534c8028fe2aa8f86b75e5d
Carlos Tovar <cartovarc@gmail.com>
2020-04-30 18:10:37 -0500

    backup

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-page class="flex full-width my-class">
+    <q-parallax class="my-parallax" :height="150">
+      <template v-slot:media>
+        <video
+          poster="https://cdn.quasar.dev/img/polina.jpg"
+          autoplay
+          loop
+          muted
+        >
+          <source
+            type="video/webm"
+            src="https://cdn.quasar.dev/img/polina.webm"
+          />
+          <source
+            type="video/mp4"
+            src="https://cdn.quasar.dev/img/polina.mp4"
+          />
+        </video>
+      </template>
+
+      <h3 class="text-white">Video</h3>
+    </q-parallax>
+
+    <q-separator />
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 1
+    };
+  }
+};
+</script>
+
+<style scoped>
+.my-parallax {
+  width: 100%;
+  min-height: 250px;
+}
+</style>
---
+++ src/pages/ShoppingCart.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex full-width my-class">
+    SHOPPING CART
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 1
+    };
+  }
+};
+</script>
+
+<style scoped>
+.my-parallax {
+  width: 100%;
+  min-height: 250px;
+}
+</style>
